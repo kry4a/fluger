edb104486203e3d43a870e344bcd639c2439dae6
Nikita <nikita.kruchinkin@gmail.com>
2018-03-09 00:31:31 +0300

    Fonts, parallax and a lot of other

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,10 @@
 $(function (){
   console.log('init');
+  $('.map__layer').parallax(
+    {mouseport: $("body"),xorigin: 0,yorigin: 0},
+    {xparallax: '0', yparallax: '0'},{xparallax: '30px',yparallax: '30px'},{xparallax: '45px',yparallax: '45px'},{xparallax: '60px',yparallax: '60px'}
+
+  );
 });
 
 
