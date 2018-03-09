c675f83c32350c260134665254e4d24a6a5281de
Nikita <nikita.kruchinkin@gmail.com>
2018-03-09 14:41:30 +0300

    Main page without responsive

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -1,2 +1,2 @@
 
-$(function(){console.log('init');$('.map__layer').parallax({mouseport:$("body"),xorigin:0,yorigin:0},{xparallax:'0',yparallax:'0'},{xparallax:'30px',yparallax:'30px'},{xparallax:'45px',yparallax:'45px'},{xparallax:'60px',yparallax:'60px'});});
+$(function(){console.log('init');$('.map__layer').parallax({mouseport:$("body"),xorigin:0,yorigin:0},{xparallax:'0',yparallax:'0'},{xparallax:'30px',yparallax:'30px'},{xparallax:'45px',yparallax:'45px'},{xparallax:'60px',yparallax:'60px'});$(window).scroll(function(){var header=$('.header'),main=$('.main'),scroll=$(window).scrollTop();if(scroll>=300){header.addClass('header--fixed');main.addClass('main--fixed');}else{header.removeClass('header--fixed');main.removeClass('main--fixed');}});});
